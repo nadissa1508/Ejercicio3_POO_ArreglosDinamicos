37109605a03bc1b25b7a051dad1db665f6e4f8ae
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2023-09-06 23:53:59 -0600

    terminacion del programa

--- Principal.java
+++ Principal.java
@@ -3,9 +3,192 @@
  * @author Angie Nadissa Vela López, 23764
  * @description Clase principal que funciona como la forma provisional de que el usuario interactue
  * con el programa
- * @date creación 02/09/2023 última modificación 28/08/23
+ * @date creación 02/09/2023 última modificación 06/09/23
  */
 
+import java.util.Scanner;
+
 public class Principal {
+   /**
+     * Menú principal del programa
+     */
+    public static void menu1(){
+        Scanner teclado = new Scanner(System.in);
+        Universidad universidad = new Universidad();
+        int opcion = 0;
+        String opUser = "";
+
+        while (opcion != 3){
+            
+            System.out.println("\n***************************");
+            System.out.println("\nMENU PRINCIPAL");
+            System.out.println("\n***************************");
+            System.out.println("\nOpciones: \n1.Seleccionar sede \n2.Registrar sede \n3.Salir");
+            System.out.println("\nSeleccione su opción: ");
+            opUser = teclado.nextLine();
+
+            try{
+                opcion = Integer.parseInt(opUser);      
+            }catch(Exception e){
+                System.out.println("\nError, ingrese un número");
+            }
+
+            if(opcion < 1 || opcion > 3){
+                System.out.println("\nError, ingrese una opción del menú");
+            }else {
+                if(opcion == 1){
+                    if(universidad.getSedeEstudiante().equals("")){
+                        System.out.println("Error, debe registrar una sede");
+                    }else{
+                        String op = "";
+                        int op2 = 0;
+                        System.out.println(universidad.mostrarSedes());
+                        op = teclado.nextLine();
     
+                        try{
+                            op2 = Integer.parseInt(op);      
+                        }catch(Exception e){
+                            System.out.println("\nError, ingrese un número");
+                        }
+                        if(op2 > 0 ){
+                            universidad.seleccionarSede((op2-1));
+                            menu2();
+                        }
+
+                    }
+                }else if(opcion == 2){
+                    String sedee = "";
+                    System.out.println("\n\nIngrese la nueva sede de la Universidad: ");
+                    sedee = teclado.nextLine();
+                    universidad.registrarSede(sedee);
+                
+                }
+                
+
+            }
+        }   
+    }
+
+    public static void menu2(){
+
+        Scanner teclado = new Scanner(System.in);
+        Universidad universidad = new Universidad();
+        int opcion = 0;
+        String opUser = "";
+
+        while (opcion != 4){
+            
+            System.out.println("\n\n***************************");
+            System.out.println("\nAdministración de la Universidad");
+            System.out.println("\n***************************");
+            System.out.println("\nOpciones: \n1.Registrar estudiante \n2.Registrar notas \n3.Mostrar estadisticas \n4.Regresar");
+            System.out.println("\nSeleccione su opción: ");
+            opUser = teclado.nextLine();
+
+            try{
+                opcion = Integer.parseInt(opUser);      
+            }catch(Exception e){
+                System.out.println("\nError, ingrese un número");
+            }
+
+            if(opcion < 1 || opcion > 4){
+                System.out.println("\nError, ingrese una opción del menú");
+            }else {
+                if(opcion == 1){
+                            
+                    System.out.println("\nRegistrar estudiante");
+                    System.out.println("\n***************************");
+                    String nombre = "", apellido = "", fechaNacimiento = "", correo= "", carnetEst = "";
+                    int carnet = 0;
+            
+                    System.out.println("\n\nIngrese nombre: ");
+                    nombre = teclado.nextLine();
+            
+                    System.out.println("\nIngrese apellido: ");
+                    apellido = teclado.nextLine();
+
+                    System.out.println("\nIngrese fecha de nacimiento: ");
+                    fechaNacimiento = teclado.nextLine();
+
+                    System.out.println("\nIngrese correo: ");
+                    correo = teclado.nextLine();
+            
+                    System.out.println("\nIngrese carnet: ");
+                    carnetEst = teclado.nextLine();  
+            
+                    try{
+                        carnet = Integer.parseInt(carnetEst);
+                    }catch(Exception e){
+                        System.out.println("\nError, debe ingresar un código numérico");
+                    }
+                    
+                    if(carnet > 0){
+                        universidad.registrarEstudiante(nombre, apellido, carnet, fechaNacimiento, correo);
+                    }else{
+                        System.out.println("\nError, carnet invalido");
+                    }
+    
+        
+                }else if(opcion == 2){
+                    System.out.println("\n\nCursos");
+                    System.out.println("\n1.Matematica \n2.Lenguaje \n3.Quimica \n4.Fisica \n5.Comprension Lectora 6.\nEstadistica");
+                    String op3 = "";
+                    int opcion2 = 0;
+                    System.out.println("\nElija una opción: ");
+                    op3 = teclado.nextLine();
+                    try{
+                        opcion2 = Integer.parseInt(op3);      
+                    }catch(Exception e){
+                        System.out.println("\nError, ingrese un número");
+                    }
+                    if(opcion2 < 1 || opcion2 > 6){
+                        System.out.println("\nError, ingrese una opción del menú");
+                    }else{
+                        
+                        String carnetEst = "", nota = "";
+                        int carnet = 0, nota2 = 0;
+
+                        System.out.println("\n\nIngrese carnet del estudiante: ");
+                        carnetEst = teclado.nextLine();
+
+                        try{
+                            carnet = Integer.parseInt(carnetEst);      
+                        }catch(Exception e){
+                            System.out.println("\nError, ingrese un número");
+                        }
+
+                        System.out.println("\n\nIngrese nota del estudiante: ");
+                        nota = teclado.nextLine();
+
+                        try{
+                            nota2 = Integer.parseInt(nota);      
+                        }catch(Exception e){
+                            System.out.println("\nError, ingrese un número");
+                        }
+
+                        if(nota2 > -1 && nota2 < 101){
+                            universidad.registrarNotas(carnet, universidad.devolverCurso(opcion2), nota2);
+                        }else{
+                            System.out.println("\nError, nota invalida");
+                        }
+
+                    }
+                }else if(opcion == 3){
+                    universidad.mostrarEstadisticas();
+                }
+
+            }
+        }
+
+
+    }
+    
+    
+    /** 
+     * @param args
+     */
+    public static void main(String[] args){
+        //iniciar el menu principal del juego
+        menu1();
+    } 
 }
--- Universidad.java
+++ Universidad.java
@@ -3,10 +3,12 @@
  * @author Angie Nadissa Vela López, 23764
  * @description Clase que administra el registro de estudiantes, asignación de notas 
  * y datos estadísticos de una universidad
- * @date creación 02/09/2023 última modificación 28/08/23
+ * @date creación 02/09/2023 última modificación 06/09/23
  */
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 
 public class Universidad {
@@ -21,12 +23,26 @@ public class Universidad {
         arrayEstudiante = new ArrayList<Estudiante>();
     }
     
+    
+    /** 
+     * @param nombre
+     * @param apellido
+     * @param carnet
+     * @param fechaNacimiento
+     * @param correo
+     */
     public void registrarEstudiante(String nombre, String apellido, int carnet, String fechaNacimiento, String correo) {
         Estudiante nuevoEstudiante = new Estudiante(nombre, apellido, carnet, fechaNacimiento, correo, sedeEstudiante);
         arrayEstudiante.add(nuevoEstudiante);
         System.out.println("Estudiante registrado correctamente! ");
     }
 
+    
+    /** 
+     * @param carnet
+     * @param curso
+     * @return boolean
+     */
     //si devuelve false entonces si se puede guardar la nota, porque no existe en la info del estudiante
     //si devuelve true no se puede guardar, el etudiante ya realizó el examen
     public boolean validarRegistroNotas(int carnet, String curso) {
@@ -40,10 +56,41 @@ public class Universidad {
         return flag;
     }
 
+    
+    /** 
+     * @param opcion
+     * @return String
+     */
+    public String devolverCurso(int opcion){
+        String curso = "";
+
+        if(opcion == 1){
+            curso = "Matematica";
+        }else if(opcion == 2){
+            curso = "Lenguaje";
+        }else if(opcion == 3){
+            curso = "Quimica";
+        }else if(opcion == 4){
+            curso = "Fisica";
+        }else if(opcion == 5){
+            curso = "comprensionLec";
+        }else if(opcion == 6){
+            curso = "Estadistica";
+        }
+
+        return curso;
+    }
+
+    
+    /** 
+     * @param carnet
+     * @param curso
+     * @param nota
+     */
     public void registrarNotas(int carnet, String curso, int nota) {
 
         if(buscarEstudiante(carnet)){
-            if(!validarRegistroNotas(carnet, curso)) {
+            if(validarRegistroNotas(carnet, curso)) {
 
                 Examen nuevoExamen = new Examen(curso, nota);
                 //ver si esto funciona
@@ -57,16 +104,26 @@ public class Universidad {
         }
     }
 
+    
+    /** 
+     * @param sede
+     */
     public void registrarSede(String sede){
         if(!sede.equals("")){
             arraySede.add(sede);
             System.out.println("Sede registrada correctamente!");
+            sedeEstudiante = sede;
         }else{
             System.out.println("Error, campo vacío!");
         }
         
     }
 
+    
+    /** 
+     * @param carnet
+     * @return boolean
+     */
     //indica si el estudiante esta registrado en la U para poder guardar su nota
     public boolean buscarEstudiante(int carnet){
         boolean flag = false;
@@ -78,6 +135,11 @@ public class Universidad {
         return flag;
     }
 
+    
+    /** 
+     * @param carnet
+     * @return int
+     */
     public int buscarIndexEstudiante(int carnet){
         int index = -1;
         for (int i = 0; i < arrayEstudiante.size(); i++){
@@ -88,7 +150,12 @@ public class Universidad {
         return index;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String mostrarSedes(){
+        sedeEstudiante = "";
         String cadenaSede = "";
         for (int i = 0 ; i < arraySede.size(); i++){
             cadenaSede += (i+1) + ". " + arraySede.get(i) + "\n";
@@ -96,10 +163,78 @@ public class Universidad {
         return cadenaSede;
     }
 
+    
+    /** 
+     * @param indexSede
+     */
+    public void seleccionarSede(int indexSede){
+        sedeEstudiante = arraySede.get(indexSede);   
+    }
+
     public void mostrarEstadisticas() {
+        System.out.println("\nESTADISTICAS GENERALES");
+        System.out.println("\n*************************");
+
+        System.out.println("\n\nMatematica");
+        System.out.println("\nNota más alta: " + notaMasAlta("Matematica"));
+        System.out.println("\nNota más baja: " + notaMasBaja("Matematica"));
+        System.out.println("\nPromedio: " + calcularPromedio("Matematica"));
+        System.out.println("\nMediana: " + calcularMediana("Matematica"));
+        System.out.println("\nModa: " + calcularModa("Matematica"));
+        System.out.println("\nDesviación estándar: " + calcularDesviacion("Matematica"));
+
+        System.out.println("\n\nLenguaje");
+
+        System.out.println("\nNota más alta: " + notaMasAlta("Lenguaje"));
+        System.out.println("\nNota más baja: " + notaMasBaja("Lenguaje"));
+        System.out.println("\nPromedio: " + calcularPromedio("Lenguaje"));
+        System.out.println("\nMediana: " + calcularMediana("Lenguaje"));
+        System.out.println("\nModa: " + calcularModa("Lenguaje"));
+        System.out.println("\nDesviación estándar: " + calcularDesviacion("Lenguaje"));
+
+        System.out.println("\n\nQuimica");
+
+        System.out.println("\nNota más alta: " + notaMasAlta("Quimica"));
+        System.out.println("\nNota más baja: " + notaMasBaja("Quimica"));
+        System.out.println("\nPromedio: " + calcularPromedio("Quimica"));
+        System.out.println("\nMediana: " + calcularMediana("Quimica"));
+        System.out.println("\nModa: " + calcularModa("Quimica"));
+        System.out.println("\nDesviación estándar: " + calcularDesviacion("Quimica"));
+
+        System.out.println("\n\nFisica");
+
+        System.out.println("\nNota más alta: " + notaMasAlta("Fisica"));
+        System.out.println("\nNota más baja: " + notaMasBaja("Fisica"));
+        System.out.println("\nPromedio: " + calcularPromedio("Fisica"));
+        System.out.println("\nMediana: " + calcularMediana("Fisica"));
+        System.out.println("\nModa: " + calcularModa("Fisica"));
+        System.out.println("\nDesviación estándar: " + calcularDesviacion("Fisica"));
+
+        System.out.println("\n\nComprension Lectora");
+
+        System.out.println("\nNota más alta: " + notaMasAlta("comprensionLec"));
+        System.out.println("\nNota más baja: " + notaMasBaja("comprensionLec"));
+        System.out.println("\nPromedio: " + calcularPromedio("comprensionLec"));
+        System.out.println("\nMediana: " + calcularMediana("comprensionLec"));
+        System.out.println("\nModa: " + calcularModa("comprensionLec"));
+        System.out.println("\nDesviación estándar: " + calcularDesviacion("comprensionLec"));
+
+        System.out.println("\n\nEstadistica");
+
+        System.out.println("\nNota más alta: " + notaMasAlta("Estadistica"));
+        System.out.println("\nNota más baja: " + notaMasBaja("Estadistica"));
+        System.out.println("\nPromedio: " + calcularPromedio("Estadistica"));
+        System.out.println("\nMediana: " + calcularMediana("Estadistica"));
+        System.out.println("\nModa: " + calcularModa("Estadistica"));
+        System.out.println("\nDesviación estándar: " + calcularDesviacion("Estadistica"));
 
     }
 
+    
+    /** 
+     * @param curso
+     * @return int
+     */
     //todo esto filtra por sedeESTUDIANTE
     public int notaMasAlta(String curso) {
         int nota = 0;
@@ -114,6 +249,11 @@ public class Universidad {
         return nota;
     }
 
+    
+    /** 
+     * @param curso
+     * @return int
+     */
     public int notaMasBaja(String curso) {
         int nota = 0;
         nota = arrayEstudiante.get(0).getArrayExamenes().get(0).getNota();
@@ -127,6 +267,11 @@ public class Universidad {
         return nota;
     }
 
+    
+    /** 
+     * @param curso
+     * @return float
+     */
     public float calcularPromedio(String curso) {
         float prom = 0.0f;
         for(int  i = 0; i < arrayEstudiante.size(); i++){
@@ -137,6 +282,11 @@ public class Universidad {
         return prom;
     }
 
+    
+    /** 
+     * @param curso
+     * @return int
+     */
     public int calcularMediana(String curso) {
         int mediana = 0;
         ArrayList<Integer> notas = new ArrayList<>();
@@ -145,7 +295,7 @@ public class Universidad {
                 notas.add(arrayEstudiante.get(i).getArrayExamenes().get(i).getNota());
             }
         }
-        Arrays.sort(notas);
+        Collections.sort(notas);
         int n = notas.size();
 
         if (n % 2 == 0) {
@@ -158,6 +308,11 @@ public class Universidad {
         return mediana;
     }
 
+    
+    /** 
+     * @param curso
+     * @return int
+     */
     public int calcularModa(String curso){
 
         int maximoNumRepeticiones = 0;
@@ -186,12 +341,45 @@ public class Universidad {
         return moda;
     } 
 
+    
+    /** 
+     * @param curso
+     * @return float
+     */
+
+
+
     public float calcularDesviacion(String curso) {
-        return 0.0f;
+        float desviacion = 0.0f;
+        ArrayList<Integer> notas = new ArrayList<>();
+        for(int  i = 0; i < arrayEstudiante.size(); i++){
+            //verificar si debo modificar .getArrayExamenes.get(0)
+            if(curso.equals(arrayEstudiante.get(i).getArrayExamenes().get(i).getCurso()) && sedeEstudiante.equals(arrayEstudiante.get(i).getSede())){
+                notas.add(arrayEstudiante.get(i).getArrayExamenes().get(i).getNota());
+            }
+        }
+
+        float promedio = calcularPromedio(curso);
+
+        float sumaDiferenciasCuadradas = 0;
+        for (Integer n : notas) {
+            float diferencia = n - promedio;
+            sumaDiferenciasCuadradas += diferencia * diferencia;
+        }
+
+        // Calcular la desviación estándar
+        desviacion = (float) Math.sqrt(sumaDiferenciasCuadradas / notas.size());
+
+        return desviacion;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getSedeEstudiante(){
         return sedeEstudiante;
     }
+
 
 }
